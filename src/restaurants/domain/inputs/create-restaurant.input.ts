eb89fc2b82c1608189a1cc5500a75e6f5f69edaf
Alexandre Junior <alejunior.dev@gmail.com>
2024-08-07 22:41:43 -0300

    feat: add zod validations in restaurant inputs

--- src/restaurants/domain/inputs/create-restaurant.input.ts
+++ src/restaurants/domain/inputs/create-restaurant.input.ts
@@ -1,7 +1,14 @@
-export interface CreateRestaurantInput {
-  name: string;
-  description: string;
-  location: string;
-  rating: number;
-  image_url?: string;
-}
+import { createZodDto } from 'nestjs-zod';
+import { z } from 'zod';
+
+export const createRestaurantInput = z.object({
+  name: z.string().min(1),
+  description: z.string().min(1),
+  location: z.string().min(1),
+  rating: z.number().finite().min(1).max(5),
+  image_url: z.string().optional().nullable(),
+});
+
+export class CreateRestaurantInput extends createZodDto(
+  createRestaurantInput,
+) {}
--- src/restaurants/domain/inputs/update-restaurant.input.ts
+++ src/restaurants/domain/inputs/update-restaurant.input.ts
@@ -1,7 +1,14 @@
-export interface UpdateRestaurantInput {
-  name: string;
-  description: string;
-  location: string;
-  rating: number;
-  image_url?: string;
-}
+import { createZodDto } from 'nestjs-zod';
+import { z } from 'zod';
+
+const updateRestaurantInput = z.object({
+  name: z.string().min(1),
+  description: z.string().min(1),
+  location: z.string().min(1),
+  rating: z.number().finite().min(1).max(5),
+  image_url: z.string().optional(),
+});
+
+export class UpdateRestaurantInput extends createZodDto(
+  updateRestaurantInput,
+) {}
